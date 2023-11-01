d4f2965d1379c675af066a0fa4287964ffc8cb2d
laiwenjian <61222673+laiwenjian@users.noreply.github.com>
2023-11-01 11:46:25 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,13 +6,7 @@ import streamlit as st
 
 """# Welcome to Streamlit! This is a Chatbot test!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
 
 
 with st.echo(code_location='below'):
